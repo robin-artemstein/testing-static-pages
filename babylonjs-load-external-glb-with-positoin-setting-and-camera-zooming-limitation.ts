89d64afd54231ef9a44ae149950a495eca349998
robin-artemstein <51808727+RobinArtemstein@users.noreply.github.com>
2025-10-27 19:43:24 +0800

    Update babylonjs-load-external-glb-with-positoin-setting-and-camera-zooming-limitation.ts

--- babylonjs-load-external-glb-with-positoin-setting-and-camera-zooming-limitation.ts
+++ babylonjs-load-external-glb-with-positoin-setting-and-camera-zooming-limitation.ts
@@ -5,20 +5,20 @@ class Playground {
         const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
         //Load external GLB model from GitHub
         BABYLON.SceneLoader.ImportMeshAsync("", "https://raw.githubusercontent.com/robin-artemstein/testing-static-pages/main/", "Katana4.glb", scene).then((result) => {
-                // Retrieve the loaded model (assuming the model is the first root node)
-                const model = result.meshes[0];
+             // Retrieve the loaded model (assuming the model is the first root node)
+            const model = result.meshes[0];
 
-                // Setting coordinate position for loaded model, for instance, move it to (x: -0.25, y: -01, z: 0)
-                model.position = new BABYLON.Vector3(-0.25, -0.1, 0);
+            // Setting coordinate position for loaded model, for instance, move it to (x: -0.25, y: -01, z: 0)
+            model.position = new BABYLON.Vector3(-0.25, -0.1, 0);
 
-                // Optional: Setting scaling ratio for the loaded model (if the model is too small or too large)
-                model.scaling = new BABYLON.Vector3(1, 1, 1); // Setting the scaling ratio
+            // Optional: Setting scaling ratio for the loaded model (if the model is too small or too large)
+            model.scaling = new BABYLON.Vector3(1, 1, 1); // Setting the scaling ratio
 
-                // Optional: Setting rotation for the loaded model (in radians)
-                model.rotation = new BABYLON.Vector3(0, Math.PI / 1, 0); // Rotate 90 degrees along the Y-axis.
-            }).catch((error) => {
-                console.error("Model loading failed:", error);
-            });
+            // Optional: Setting rotation for the loaded model (in radians)
+            model.rotation = new BABYLON.Vector3(0, Math.PI / 1, 0); // Rotate 90 degrees along the Y-axis.
+        }).catch((error) => {
+            console.error("Model loading failed:", error);
+        });
         //Create the the default camera in the scene.
         scene.createDefaultCameraOrLight(true, true, true);
         scene.createDefaultEnvironment();
