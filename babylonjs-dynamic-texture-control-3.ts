b9a22bb6915827e924eb65e9e9ecaefe9d6746c2
robin-artemstein <51808727+RobinArtemstein@users.noreply.github.com>
2026-03-10 20:41:57 +0800

    Create babylonjs-dynamic-texture-control-3.ts

---
+++ babylonjs-dynamic-texture-control-3.ts
@@ -0,0 +1,56 @@
+class Playground {
+    public static CreateScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement): BABYLON.Scene {
+        // Create Scene and camera
+        const scene = new BABYLON.Scene(engine);
+        scene.clearColor = new BABYLON.Color4(0, 0, 0, 1);
+        scene.createDefaultCameraOrLight(true, true, true);
+        scene.createDefaultEnvironment();
+        const camera = scene.activeCamera as BABYLON.ArcRotateCamera;
+        // Arc rotate camera setting
+        camera.alpha = -Math.PI / 2;
+        camera.beta =  Math.PI / 1;
+        camera.radius = 1.5;
+        camera.lowerRadiusLimit = 0.25;
+        camera.upperRadiusLimit = 10;
+        // Limit zooming speed for touch devices
+        camera.pinchPrecision = 0.005;
+        camera.pinchDeltaPercentage = 0.005;
+        // Limit Zooming speed for mouse
+        camera.wheelPrecision = 0.005;
+        camera.wheelDeltaPercentage = 0.005;
+        // Limit panning speed for both of mouse and touch devices
+        camera.panningSensibility = 5000; // Panning speed setting, smaller means faster panning
+        // Create a ground plane defualt model
+	    const groundWidth = 1;
+        const groundHeight = 1;
+        const ground = BABYLON.MeshBuilder.CreateGround("ground1", {width: groundWidth, height: groundHeight}, scene)
+        ground.rotation.x = -Math.PI / 2; 
+        // Create dynamic texture canvas
+        const textureResolution = 512;
+	    const textureGround = new BABYLON.DynamicTexture("dynamic texture", textureResolution, scene);   
+        // Uncomment this line of code if you want the transparent (alpha) background
+        //textureGround.hasAlpha = true;
+	    const textureContext = textureGround.getContext();
+        
+        // Create standard material for the model
+    	const materialGround = new BABYLON.StandardMaterial("Mat", scene);    				
+	    materialGround.diffuseTexture = textureGround;
+	    ground.material = materialGround;
+        // Load image
+	    const img = new Image();
+        const imgPosX = 3;
+        const imgPosY = 5;
+        const imgWidth = 100;
+        const imgHeight = 100;
+	    img.src = 'https://i.imgur.com/1eAmEO8.png';
+        img.crossOrigin = "Anonymous";
+	    img.onload = function() {
+            // Add image to dynamic texture
+            textureContext.drawImage(this, imgPosX, imgPosY, imgWidth, imgHeight)
+		    textureGround.update();	
+        }	
+
+        return scene;
+    }
+}
+export { Playground };
