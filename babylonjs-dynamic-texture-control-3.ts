aa3bb73a392bcf80a7522ebb06f6f80e07cee6e9
robin-artemstein <51808727+RobinArtemstein@users.noreply.github.com>
2026-03-11 11:39:56 +0800

    Update babylonjs-dynamic-texture-control-3.ts

--- babylonjs-dynamic-texture-control-3.ts
+++ babylonjs-dynamic-texture-control-3.ts
@@ -2,24 +2,14 @@ class Playground {
     public static CreateScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement): BABYLON.Scene {
         // Create Scene and camera
         const scene = new BABYLON.Scene(engine);
-        scene.clearColor = new BABYLON.Color4(0, 0, 0, 1);
+        scene.clearColor = new BABYLON.Color4(0.7, 0.5, 0.5);
         scene.createDefaultCameraOrLight(true, true, true);
         scene.createDefaultEnvironment();
         const camera = scene.activeCamera as BABYLON.ArcRotateCamera;
         // Arc rotate camera setting
         camera.alpha = -Math.PI / 2;
         camera.beta =  Math.PI / 1;
-        camera.radius = 1.5;
-        camera.lowerRadiusLimit = 0.25;
-        camera.upperRadiusLimit = 10;
-        // Limit zooming speed for touch devices
-        camera.pinchPrecision = 0.005;
-        camera.pinchDeltaPercentage = 0.005;
-        // Limit Zooming speed for mouse
-        camera.wheelPrecision = 0.005;
-        camera.wheelDeltaPercentage = 0.005;
-        // Limit panning speed for both of mouse and touch devices
-        camera.panningSensibility = 5000; // Panning speed setting, smaller means faster panning
+        
         // Create a ground plane defualt model
 	    const groundWidth = 1;
         const groundHeight = 1;
@@ -39,8 +29,9 @@ class Playground {
         materialGround.albedoColor = new BABYLON.Color3(1, 1, 1);
         materialGround.metallic = 1.0;
         materialGround.roughness = 0.1
+        materialGround.transparencyMode = BABYLON.PBRMaterial.PBRMATERIAL_ALPHABLEND;
+        materialGround.useAlphaFromAlbedoTexture = true;
 	    const textureContext = textureGround.getContext();
-
         // Load image
         const imgPosX = 3;
         const imgPosY = 5;
